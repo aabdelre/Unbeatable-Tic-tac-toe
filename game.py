e2326413c859c42403a9e25bcd0e4532e0e93570
Ahmed Abdelrehim <88915626+aabdelre@users.noreply.github.com>
2022-12-20 00:52:13 +0200

    Initialize Gama Class

---
+++ game.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+def switch_truns(turn):
+    """Returns the other color"""
+    assert turn in ['X', 'O']
+    return 'X' if turn == 'O' else 'O'
+
+class GameMove():
+    def __init__(self, r : int, c : int, player : str):
+        self.pair = (r, c)
+        self.player = player
+    
+    def __str__(self) -> str:
+        return "{} placing at {}".format(self.player, self.pair)
+    
+    def __repr__(self) -> str:
+        return "({} {})".format(self.pair[0], self.pair[1])
+
+    def __eq__(self, other):
+        return self.pair == other.pair and self.player == other.player
+    
+    def __hash__(self):
+        return hash(self.__str__()) # Not sure if this how to do it yet
+
+class GameState():
+    def __init__(self, test_board = [['E'] * 3 for _ in range(3)]):
+        self.board = test_board #[['E'] * 3 for _ in range(3)]
+        self.current = 'X'
+        self.move_number = 0
+
+    def full_board(self):
+        for row in self.board:
+            for entry in row:
+                if entry == 'E':
+                    return False
+        return True
+    
+    def check_rows(self, board):
+        """Takes board as a parameter since it is being transposed and stored board
+        should not change."""
+        for row in board:
+            if len(set(row)) == 1 and row[0] != 'E':
+                return row[0]
+        return 0
+
+    def check_digonal(self):
+        if len(set([self.board[i][i] for i in range(len(self.board))])) == 1 and self.board[0][0] != 'E':
+            return self.board[0][0]
+        if len(set([self.board[i][len(self.board) - i - 1] for i in range(len(self.board))])) == 1 and self.board[0][len(self.board) - 1] != 'E':
+            return self.board[0][len(self.board) - 1]
+        return 0
+
+    def evaluate(self):
+        """d = draw, X = X won, Y = Y won, n = no one won yet"""
+        for board in [self.board, np.transpose(self.board)]:
+            result = self.check_rows(board)
+            if result and result != 'E':
+                return result
+        #print("It's a diagonal win!")
+        result = self.check_digonal()
+        return result if result else 'd' if self.full_board() else 'n'
+        
+    def game_over(self):
+        return self.available_moves() == [] or self.evaluate() != 'n'
+
+    def winner(self):
+        assert self.game_over()
+        result = self.evaluate()
+        return 'draw' if result == 'd' else 'X' if result == 'X' else 'O'
+
+    def available_moves(self):
+        return False
+    
+def main():
+    #move = GameMove(1, 1, 'X')
+    #print(move)
+    test_board = [['E', 'X', 'X'],
+                  ['E', 'X', 'X'],
+                  ['X', 'O', 'E']]
+    game = GameState(test_board)
+    #print(game.winner())
+if __name__ == "__main__":
+    main()
