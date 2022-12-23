fbad1db8ac4ef6240bd0ac25cec54b2152772de5
Ahmed Abdelrehim <88915626+aabdelre@users.noreply.github.com>
2022-12-23 22:43:01 +0200

    minmax & simple heuristic

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 """Observations: pretty hard to get a draw if two random players are matched up."""
-import copy, time, random, numpy as np
+import copy, time, random, sys, numpy as np
 
 def switch_truns(turn):
     assert turn in ['X', 'O']
@@ -206,7 +206,75 @@ class HumanPlayer():
         if move in available:
             return move
 
+class SearchNode():
+    def __init__(self, state, depth, move):
+        self.game_state = state
+        self.depth = depth
+        self.move = move
 
+    def is_terminal(self):
+        return self.game_state.game_over()
+
+    def utility(self, turn):
+        value = 0
+        turn_incrementer = 1
+        opp_turn_incrementer = 1
+        for row in self.game_state.board:
+            for entry in row:
+                turn_incrementer *= 10 if entry == turn else turn_incrementer
+                opp_turn_incrementer *= 10 if entry == turn else opp_turn_incrementer
+            turn_incrementer = 1
+            opp_turn_incrementer = 1
+            value += turn_incrementer - opp_turn_incrementer
+
+        turn_incrementer = 1
+        opp_turn_incrementer = 1
+        diagonal = [self.game_state.board[i][i] for i in range(len(self.game_state.board))]
+        for entry in diagonal:
+            turn_incrementer *= 10 if entry == turn else turn_incrementer
+            opp_turn_incrementer *= 10 if entry == turn else opp_turn_incrementer
+        value += turn_incrementer - opp_turn_incrementer
+
+        turn_incrementer = 1
+        opp_turn_incrementer = 1
+        rev_diagonal = [self.game_state.board[i][len(self.game_state.board) - i - 1] for i in range(len(self.game_state.board))]
+        for entry in rev_diagonal:
+            turn_incrementer *= 10 if entry == turn else turn_incrementer
+            opp_turn_incrementer *= 10 if entry == turn else opp_turn_incrementer
+        value += turn_incrementer - opp_turn_incrementer
+        return value
+    
+class MinMaxPlayer():
+    def __init__(self, turn):
+        assert turn in ["X", "O"]
+        self.turn = turn
+
+    def __str__(self):
+        return "MiniMax"
+
+    def make_move(self, state, remaining_time):
+        node = SearchNode(state, 0, (None, None))
+        return self.minimax(node, 7, node.game_state.current)[1]
+    
+    def minimax(self, node, depth, player):
+        if depth == 0 or node.is_terminal():
+            return (node.utility(self.turn), None)
+
+        if player == self.turn:
+            best = (-sys.maxsize, None)
+            for move in node.game_state.available_moves():
+                new_state = node.game_state.apply_move(move)
+                new_node = SearchNode(new_state, node.depth + 1, move)
+                best = (max(best[0], self.minimax(new_node, depth - 1, switch_truns(node.game_state.current))[0]), move)
+            return best
+    
+        else:
+            best = (sys.maxsize, None)
+            for move in node.game_state.available_moves():
+                new_state = node.game_state.apply_move(move)
+                new_node = SearchNode(new_state, node.depth + 1, move)
+                best = (min(best[0], self.minimax(new_node, depth - 1, switch_truns(node.game_state.current))[0]), move)
+            return best
 
 def main():
     #move = GameMove(1, 1, 'X')
@@ -214,7 +282,7 @@ def main():
     #test_board = [['X', 'X', 'X'],
     #              ['E', 'O', 'X'],
     #              ['X', 'O', 'E']]
-    game = TicTacToeGame(RandomPlayer('X'), HumanPlayer('O'),
+    game = TicTacToeGame(RandomPlayer('X'), MinMaxPlayer('O'),
                         x_name = "Random X", o_name = "Random O",
                         verbose = True,
                         lose_when_out_of_time = True)
